bee2529e838b200de847835e947382a438eec7a1
cindyzht <cindyzht@yeah.net>
2020-12-08 11:53:28 +0800

    add socket source demo

--- src/main/java/org/learn/flink/connector/socket/SocketStreamJob.java
+++ src/main/java/org/learn/flink/connector/socket/SocketStreamJob.java
@@ -16,9 +16,9 @@ public class SocketStreamJob {
         //处理输入数据流，转换为StudentInfo类型，方便后续处理
         SingleOutputStreamOperator<ClickEvent> dataStreamStudent = textStudent.flatMap(new FlatMapFunction<String, StudentInfo>() {
             @Override
-            public void flatMap(String s, Collector<ClickEvent> collector){
+            public void flatMap(String s, Collector<ClickEvent> collector) {
                 String infos[] = s.split(",");
-                if(StringUtils.isNotBlank(s) && infos.length==5){
+                if (StringUtils.isNotBlank(s) && infos.length == 5) {
                     ClickEvent clickEvent = new ClickEvent();
                     clickEvent.setName(infos[0]);
                     clickEvent.setSex(infos[1]);
@@ -31,11 +31,13 @@ public class SocketStreamJob {
 
         DataStream<clickEvent> dataStream = dataStreamStudent.assignTimestampsAndWatermarks(new AssignerWithPeriodicWatermarks<StudentInfo>() {
             private final long maxTimeLag = 5000; // 5 seconds
+
             @Nullable
             @Override
             public Watermark getCurrentWatermark() {
                 return new Watermark(System.currentTimeMillis() - maxTimeLag);
             }
+
             @Override
             public long extractTimestamp(StudentInfo studentInfo, long l) {
                 return studentInfo.getTimestamp();
@@ -43,9 +45,9 @@ public class SocketStreamJob {
         });
 
         //注册dataStreamStudent流到表中，表名为：studentInfo
-        tEnv.registerDataStream("studentInfo",dataStream,"name,sex,course,score,timestamp,sysDate.rowtime");
+        tEnv.registerDataStream("studentInfo", dataStream, "name,sex,course,score,timestamp,sysDate.rowtime");
 
-        tEnv.registerFunction("utc2local",new UTC2Local());
+        tEnv.registerFunction("utc2local", new UTC2Local());
 
         //GroupBy Window Aggregation 根据name分组，统计学科数量
         Table groupWindAggrTable = tEnv.sqlQuery("user,tumble_end(ctime,interval '1' hours) as end_time,count(url) as cnt\n" +
@@ -67,7 +69,9 @@ public class SocketStreamJob {
     class UTC2Local extends ScalarFunction {
 
         public Timestamp eval(Timestamp s) {
-            long timestamp = s.getTime() + 28800000; flink默认的是UTC时间，我们的时区是东八区，时间戳需要增加八个小时
+            long timestamp = s.getTime() + 28800000;
+            //flink默认的是UTC时间，我们的时区是东八区，时间戳需要增加八个小时
             return new Timestamp(timestamp);
         }
     }
+}
